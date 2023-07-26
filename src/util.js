623b87f361cffcb359f2de52c5382bc196e95175
s0913191 <s0913191@gmail.com>
2023-07-25 22:25:14 -0400

    deleted all

--- src/util.js
+++
@@ -1,8 +0,0 @@
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-
-function formatId(x){
-    return x.replace(/\s/g, "-").replace(/[()]/g, "");
-}
